45b5a89ba2bf09fc4fde1faf2d3d5fe30e8e96fe
BoghianIulian <iulianboghian29@gmail.com>
2024-12-04 20:25:08 +0200

    return profil financiar pt user logat

--- src/main/java/com/example/moneycalling_spring/Controller/ProfilFinanciarController.java
+++ src/main/java/com/example/moneycalling_spring/Controller/ProfilFinanciarController.java
@@ -1,6 +1,8 @@
 package com.example.moneycalling_spring.Controller;
 
 import com.example.moneycalling_spring.Domain.ProfilFinanciar;
+import com.example.moneycalling_spring.Domain.Utilizator;
+import com.example.moneycalling_spring.Security.JwtUtil;
 import com.example.moneycalling_spring.Service.ProfilFinanciarService;
 import com.example.moneycalling_spring.Service.UtilizatorService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/profiluri-financiare")
@@ -18,21 +21,16 @@ public class ProfilFinanciarController {
     private final ProfilFinanciarService profilFinanciarService;
     private final UtilizatorService utilizatorService;
 
+    private final JwtUtil jwtUtil;
+
     @Autowired
-    public ProfilFinanciarController(ProfilFinanciarService profilFinanciarService, UtilizatorService utilizatorService)
+    public ProfilFinanciarController(ProfilFinanciarService profilFinanciarService, UtilizatorService utilizatorService, JwtUtil jwtUtil)
     {
         this.utilizatorService = utilizatorService;
         this.profilFinanciarService = profilFinanciarService;
+        this.jwtUtil = jwtUtil;
     }
 
-    // Endpoint pentru a salva sau actualiza un profil financiar
-   /* @PostMapping
-    @Operation(summary = "adauga un nou profil")
-    public ResponseEntity<ProfilFinanciar> saveProfilFinanciar(@RequestBody CreareProfilDTO profilFinanciar) {
-
-        ProfilFinanciar savedProfil = profilFinanciarService.saveProfilFinanciar(profilFinanciar);
-        return new ResponseEntity<>(savedProfil, HttpStatus.CREATED);
-    }*/
 
     // Endpoint pentru a șterge un profil financiar după ID
     @DeleteMapping("/{id}")
@@ -44,11 +42,28 @@ public class ProfilFinanciarController {
     }
 
     // Endpoint pentru a obține toate profilurile financiare
-    @GetMapping
+    @GetMapping("/api/profiluri-financiare")
     @Operation(summary = "returneaza toate profilurile")
     public ResponseEntity<List<ProfilFinanciar>> getAllProfiluriFinanciare() {
         List<ProfilFinanciar> profiluri = profilFinanciarService.getAllProfiluriFinanciare();
         return new ResponseEntity<>(profiluri, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/profiluri-financiare/token")
+    @Operation(summary = "returneaza profil financiar dupa user logat")
+    public ResponseEntity<ProfilFinanciar> getProfilFinanciarByUserLogat(@RequestHeader("Authorization") String token)
+    {
+        int userId = jwtUtil.getUserIdByToken(token);
+
+        Optional<Utilizator> utilizatorOptional = utilizatorService.getById(userId);
+        if (utilizatorOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Utilizator utilizator = utilizatorOptional.get();
+
+        ProfilFinanciar profil = utilizator.getProfil();
+
+        return new ResponseEntity<>(profil,HttpStatus.OK);
     }
 
     // Endpoint pentru a șterge toate profilurile financiare
