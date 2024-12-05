e9d91bf4156784701b57472998555a6c9b3907b0
BoghianIulian <iulianboghian29@gmail.com>
2024-12-05 20:40:40 +0200

    Changes by Alexandra

--- src/main/java/com/example/moneycalling_spring/Service/RaportService.java
+++ src/main/java/com/example/moneycalling_spring/Service/RaportService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 @Service
 public class RaportService {
@@ -20,21 +23,19 @@ public class RaportService {
     private final Map<Integer, Float> chiriiPropuse = new ConcurrentHashMap<>();
 
     @Autowired
-    public RaportService(RaportRepository rap, ProfilFinanciarRepository prof)
-    {
+    public RaportService(RaportRepository rap, ProfilFinanciarRepository prof) {
         this.raportRepository = rap;
         this.profilFinanciarRepository = prof;
     }
 
-    public Raport saveRaport(Raport raport){
+    public Raport saveRaport(Raport raport) {
         return raportRepository.save(raport);
         //aceasta metoda poate fi folosita si pentru update:
         //-daca este creata un raport cu un id nou il creeaza
         //-daca exista deja acel id,il actualizeaza
     }
 
-    public void stergeRaportById(int id)
-    {
+    public void stergeRaportById(int id) {
         raportRepository.deleteById(id);
         //sterge raport dupa id
     }
@@ -45,8 +46,7 @@ public class RaportService {
         return (procent * venit) / 100;
     }
 
-    public float sugereazaRataByVenit(float suma, int ani)
-    {
+    public float sugereazaRataByVenit(float suma, int ani) {
         float dobanda = 0.05f;
         float sumaTotala = suma + suma * dobanda * ani;
         float numarRate = ani * 12;
@@ -54,17 +54,29 @@ public class RaportService {
         return valoareRate;
     }
 
-    public List<Raport> getAllRapoarteByDiagrama(Diagrama diagrama)
-    {
+    public List<Raport> getAllRapoarteByDiagrama(Diagrama diagrama) {
         return raportRepository.findByDiagrama(diagrama);
         //returneaza toate rapoartele dupa o diagrama
     }
-    public void deleteAllRapoarteByDiagrama(Diagrama diagrama)
-    {
+
+    public void deleteAllRapoarteByDiagrama(Diagrama diagrama) {
         raportRepository.deleteAllByDiagrama(diagrama);
     }
-    public Optional<Raport> getById(int id)
-    {
+
+    public Optional<Raport> getById(int id) {
         return raportRepository.findById(id);
+    }
+
+
+    public void stocheazaChiriePropusa(int userId, float chiriePropusa) {
+        chiriiPropuse.put(userId, chiriePropusa);
+    }
+
+    public Optional<Float> getChiriePropusa(int userId) {
+        return Optional.ofNullable(chiriiPropuse.get(userId));
+    }
+
+    public void eliminaChiriePropusa(int userId) {
+        chiriiPropuse.remove(userId);
     }
 }
