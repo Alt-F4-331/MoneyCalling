94c20fca1e19ca665541c5438f9e6ec088e08873
BoghianIulian <iulianboghian29@gmail.com>
2025-01-17 15:40:06 +0200

    update

--- src/main/java/com/example/moneycalling_spring/Controller/RaportController.java
+++ src/main/java/com/example/moneycalling_spring/Controller/RaportController.java
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 @RestController
@@ -25,7 +25,6 @@ public class RaportController {
     private final DiagramaService diagramaService;
     private final CheltuialaService cheltuialaService;
     private final JwtUtil jwtutil;
-
     private final AbonamentService abonamentService;
 
     @Autowired
@@ -40,7 +39,7 @@ public class RaportController {
 
     // Endpoint pentru salvarea sau actualizarea unui raport
     @PostMapping
-    @Operation(summary = "adauga un nou raport")
+    @Operation(summary = "Adauga un nou raport")
     public ResponseEntity<RaportRequestDTO> saveRaport(@RequestBody RaportRequestDTO dto) {
         Diagrama diagrama = diagramaService.getById(dto.getIdDiagrama()).get();  // No exception handling
         Raport raport = RaportRequestDTO.mapToEntity(dto, diagrama);
@@ -50,17 +49,17 @@ public class RaportController {
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
-    // Endpoint pentru ștergerea unui raport după ID
+    // Endpoint pentru stergerea unui raport dupa ID
     @DeleteMapping("/{id}")
-    @Operation(summary = "sterge un raport")
+    @Operation(summary = "Sterge un raport")
     public ResponseEntity<Void> deleteRaportById(@PathVariable int id) {
         raportService.stergeRaportById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    // Endpoint pentru obținerea tuturor rapoartelor după ID-ul Diagramei
+    // Endpoint pentru obtinerea tuturor rapoartelor dupa ID-ul Diagramei
     @GetMapping("/diagrama/{idDiagrama}")
-    @Operation(summary = "returneaza toate rapoartele dupa diagrama")
+    @Operation(summary = "Returneaza toate rapoartele dupa diagrama")
     public ResponseEntity<List<RaportRequestDTO>> getAllRapoarteByIdDiagrama(@PathVariable int idDiagrama) {
         Diagrama diagrama = diagramaService.getById(idDiagrama).get();  // No exception handling
         List<Raport> rapoarte = raportService.getAllRapoarteByDiagrama(diagrama);
@@ -72,9 +71,9 @@ public class RaportController {
         return new ResponseEntity<>(responseDtos, HttpStatus.OK);
     }
 
-    // Endpoint pentru ștergerea tuturor rapoartelor după ID-ul Diagramei
+    // Endpoint pentru stergerea tuturor rapoartelor dupa ID-ul Diagramei
     @DeleteMapping("/diagrama/{idDiagrama}")
-    @Operation(summary = "sterge toate rapoartele dupa diagrama")
+    @Operation(summary = "Sterge toate rapoartele dupa diagrama")
     public ResponseEntity<Void> deleteAllRapoarteByIdDiagrama(@PathVariable int idDiagrama) {
         Diagrama diagrama = diagramaService.getById(idDiagrama).get();  // No exception handling
         raportService.deleteAllRapoarteByDiagrama(diagrama);
@@ -83,13 +82,13 @@ public class RaportController {
 
     // Endpoint pentru sugerarea chiriei pe baza venitului
     @GetMapping("/sugereaza-chirie")
-    @Operation(summary = "Sugerează chiria pe baza venitului")
+    @Operation(summary = "Sugereaza chiria pe baza venitului")
     public ResponseEntity<Float> sugereazaChirie(@RequestHeader("Authorization") String token) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
         float venit = utilizator.getProfil().getVenit();
 
-        Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get();// No exception handling
+        Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get(); // No exception handling
 
         Float procentLoc = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.LOCUINTA, 0.0f);
         float rezultat = (procentLoc * venit) / 100;
@@ -99,43 +98,37 @@ public class RaportController {
         return new ResponseEntity<>(chirieSugerata, HttpStatus.OK);
     }
 
-    // Endpoint pentru sugerarea ratei pe baza sumei și numărului de ani
-    @GetMapping("/sugerseaza-rata")
-    @Operation(summary = "Sugerează rata pe luna,dupa suma pt TRANSPORT")
+    // Endpoint pentru sugerarea ratei pe baza sumei si numarului de ani
+    @GetMapping("/sugereaza-rata")
+    @Operation(summary = "Sugereaza rata pe luna, dupa suma pentru TRANSPORT")
     public ResponseEntity<Float> sugereazaRata(@RequestHeader("Authorization") String token) {
-
         float procentRata = 66;
         int userId = jwtutil.getUserIdByToken(token);
 
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
         float venit = utilizator.getProfil().getVenit();
 
-        Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get();// No exception handling
+        Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get(); // No exception handling
 
         Float procentTransport = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.TRANSPORT, 0.0f);
         float rezultat = (procentTransport * venit) / 100;
         rezultat = Math.round(rezultat * 100) / 100.0f;
-        float rezultatFinal = (procentRata * rezultat)/100;
-        rezultatFinal = Math.round(rezultatFinal*100) / 100.0f;
-
-
+        float rezultatFinal = (procentRata * rezultat) / 100;
+        rezultatFinal = Math.round(rezultatFinal * 100) / 100.0f;
 
         return new ResponseEntity<>(rezultatFinal, HttpStatus.OK);
     }
 
     @GetMapping("/calculeaza-rata")
-    @Operation(summary = "Sugerează rata pe baza sumei și numărului de ani")
-    public ResponseEntity<Float> calculeazaRata(@RequestParam float sumaPropusa , @RequestParam int luni)
-
-    {
-        float rata = raportService.sugereazaRata(sumaPropusa,luni);
+    @Operation(summary = "Sugereaza rata pe baza sumei si numarului de ani")
+    public ResponseEntity<Float> calculeazaRata(@RequestParam float sumaPropusa, @RequestParam int luni) {
+        float rata = raportService.sugereazaRata(sumaPropusa, luni);
         return new ResponseEntity<>(rata, HttpStatus.OK);
-
     }
 
     @PostMapping("/adauga-rata")
-    @Operation(summary = "Sugerează rata pe baza sumei și numărului de ani")
-    public ResponseEntity<?> addRata(@RequestHeader("Authorization") String token,@RequestParam float rataPropusa){
+    @Operation(summary = "Sugereaza rata pe baza sumei si numarului de ani")
+    public ResponseEntity<?> addRata(@RequestHeader("Authorization") String token, @RequestParam float rataPropusa) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
 
@@ -145,43 +138,39 @@ public class RaportController {
         cheltuialaService.saveCheltuiala(ch);
 
         Float procentRamas = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.TRANSPORT, 0.0f);
-
         float procentNou = procentRamas - (rataPropusa / utilizator.getProfil().getVenit()) * 100;
 
         diagrama.getProcenteCheltuieli().put(Cheltuiala.TipCheltuiala.TRANSPORT, procentNou);
         diagramaService.saveDiagrama(diagrama);
-        return new ResponseEntity<>("Rata propusă este acceptată automat.", HttpStatus.OK);
-
-
-
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("X-Custom-Status-Text", "Request was successful")
+                .body("The proposed rate has been automatically accepted.");
     }
 
     @PostMapping("/initiaza-chirie")
-    @Operation(summary = "Inițiază procesul pentru adăugarea chiriei")
+    @Operation(summary = "Initiaza procesul pentru adaugarea chiriei")
     public ResponseEntity<String> initiazaChirie(@RequestHeader("Authorization") String token, @RequestParam float chiriePropusa) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
         float venit = utilizator.getProfil().getVenit();
 
-
         Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get();
         Float procentLoc = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.LOCUINTA, 0.0f);
         float rezultat = (procentLoc * venit) / 100;
-        rezultat = Math.round(rezultat * 100) / 100.0f;// No exception handling
+        rezultat = Math.round(rezultat * 100) / 100.0f; // No exception handling
 
         float chirieSugerata = raportService.sugereazaChirieByVenit(rezultat, diagrama);
 
-        // Verificare fără excepții, doar returnează mesaj
+        // Verificare fara exceptii, doar returneaza mesaj
         Float procentMaximLocuinta = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.LOCUINTA, 0.0f);
         float sumaMaximaLocuinta = (venit * procentMaximLocuinta) / 100;
 
         if (chiriePropusa > sumaMaximaLocuinta) {
-            return new ResponseEntity<>("Chiria propusă depășește limita maximă permisă pentru locuință! Maxim permis: " + sumaMaximaLocuinta, HttpStatus.OK);
+            return new ResponseEntity<>("Proposed rent exceeds the maximum limit allowed for housing! Maximum allowed: " + sumaMaximaLocuinta, HttpStatus.OK);
         }
 
         if (chiriePropusa > chirieSugerata) {
-            raportService.stocheazaChiriePropusa(userId, chiriePropusa);
-            return new ResponseEntity<>("Chiria propusă depășește suma sugerată de " + chirieSugerata + ". Continuați? Apelați endpoint-ul /confirma-chirie pentru a confirma sau refuza.", HttpStatus.OK);
+            return new ResponseEntity<>("Proposed rent exceeds the suggested amount of " + chirieSugerata + ". Continue? Call the /confirma-chirie endpoint to confirm or decline.", HttpStatus.OK);
         }
 
         Cheltuiala ch = new Cheltuiala(cheltuialaService.getFirstAvailableId(), "chirie", chiriePropusa, Cheltuiala.TipCheltuiala.LOCUINTA, diagrama);
@@ -192,49 +181,43 @@ public class RaportController {
 
         diagrama.getProcenteCheltuieli().put(Cheltuiala.TipCheltuiala.LOCUINTA, procentNou);
         diagramaService.saveDiagrama(diagrama);
-        return new ResponseEntity<>("Chiria propusă este acceptată automat.", HttpStatus.OK);
+        return new ResponseEntity<>("Proposed rent has been automatically accepted.", HttpStatus.OK);
     }
 
     @PostMapping("/confirma-chirie")
-    @Operation(summary = "Confirmă sau respinge chiria propusă")
+    @Operation(summary = "Confirma sau respinge chiria propusa")
     public ResponseEntity<String> confirmaChirie(@RequestHeader("Authorization") String token, @RequestParam float chiriePropusa) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
 
-
         Diagrama diagrama = diagramaService.getDiagramaActivaByUtilizator(utilizator).get();  // No exception handling
 
+        Cheltuiala ch = new Cheltuiala(cheltuialaService.getFirstAvailableId(), "chirie", chiriePropusa, Cheltuiala.TipCheltuiala.LOCUINTA, diagrama);
+        cheltuialaService.saveCheltuiala(ch);
+        Float procentRamas = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.LOCUINTA, 0.0f);
 
-            Cheltuiala ch = new Cheltuiala(cheltuialaService.getFirstAvailableId(), "chirie", chiriePropusa, Cheltuiala.TipCheltuiala.LOCUINTA, diagrama);
-            cheltuialaService.saveCheltuiala(ch);
-            Float procentRamas = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.LOCUINTA, 0.0f);
+        float procentNou = procentRamas - (chiriePropusa / utilizator.getProfil().getVenit()) * 100;
 
-            float procentNou = procentRamas - (chiriePropusa / utilizator.getProfil().getVenit()) * 100;
-
-            diagrama.getProcenteCheltuieli().put(Cheltuiala.TipCheltuiala.LOCUINTA, procentNou);
-            diagramaService.saveDiagrama(diagrama);
-            return new ResponseEntity<>("Chiria propusă a fost acceptată și adăugată.", HttpStatus.OK);
-
-        }
+        diagrama.getProcenteCheltuieli().put(Cheltuiala.TipCheltuiala.LOCUINTA, procentNou);
+        diagramaService.saveDiagrama(diagrama);
+        return new ResponseEntity<>("Proposed rent has been accepted and added.", HttpStatus.OK);
+    }
 
     @GetMapping("/sugereaza-vacanta")
-    @Operation(summary = "Sugerează alocarea bugetului pentru vacanță")
+    @Operation(summary = "Sugereaza alocarea bugetului pentru vacanta")
     public ResponseEntity<?> sugereazaBugetVacanta(@RequestHeader("Authorization") String token, @RequestParam int nrZile, @RequestParam float bugetTotal) {
-
-        System.out.println(nrZile);
-
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();
         float containerEconomii = utilizator.getProfil().getContainerEconomii();
 
         if (bugetTotal <= 0) {
-            return new ResponseEntity<>(Map.of("message", "Bugetul este negativ"), HttpStatus.BAD_REQUEST); // Buget invalid
+            return new ResponseEntity<>(Map.of("message", "Budget is negative"), HttpStatus.BAD_REQUEST); // Buget invalid
         }
         if (bugetTotal > containerEconomii) {
-            return new ResponseEntity<>(Map.of("message", "Bugetul trebuie sa fie mai mic decat suma din containerul de economii"), HttpStatus.BAD_REQUEST); // Buget invalid
+            return new ResponseEntity<>(Map.of("message", "Budget must be less than the amount in the savings container"), HttpStatus.BAD_REQUEST); // Buget invalid
         }
 
-        // Distribuția procentuală
+        // Distributia procentuala
         float procentTransport = 0.3f; // 30% transport
         float procentCazare = 0.4f;    // 40% cazare
         float procentAltele = 0.3f;    // 30% alte cheltuieli
@@ -244,7 +227,7 @@ public class RaportController {
         float sumaCazare = bugetTotal * procentCazare / nrZile;
         float sumaAltele = bugetTotal * procentAltele;
 
-        // Rotunjirea valorilor la două zecimale
+        // Rotunjirea valorilor la doua zecimale
         sumaTransport = Math.round(sumaTransport * 100) / 100.0f;
         sumaCazare = Math.round(sumaCazare * 100) / 100.0f;
         sumaAltele = Math.round(sumaAltele * 100) / 100.0f;
@@ -258,24 +241,22 @@ public class RaportController {
         // Stocarea chiriei propuse
         raportService.stocheazaChiriePropusa(userId, bugetTotal);
 
-        // Returnează un obiect JSON cu datele relevante
+        // Returneaza un obiect JSON cu datele relevante
         return new ResponseEntity<>(Map.of(
                 "bugetDistribuit", bugetDistribuit,
-                "message", "Bugetul propus este: " + bugetDistribuit + " Doriti sa continuati? Apelați endpoint-ul /confirma-vacanta pentru a confirma sau refuza."
+                "message", "Proposed budget is: " + bugetDistribuit + " Do you want to continue? Call the /confirma-vacanta endpoint to confirm or decline."
         ), HttpStatus.OK);
     }
 
-
-
     @PostMapping("/confirma-vacanta")
-    @Operation(summary = "confirma sau respinge sumele pentru vacanta")
+    @Operation(summary = "Confirma sau respinge sumele pentru vacanta")
     public ResponseEntity<String> confirmaVacanta(@RequestHeader("Authorization") String token, @RequestParam boolean confirm) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();  // No exception handling
 
         Optional<Float> chiriePropusaOptional = raportService.getChiriePropusa(userId);
         if (chiriePropusaOptional.isEmpty()) {
-            return new ResponseEntity<>("Nu există o chirie propusă în așteptare.", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("No proposed rent pending.", HttpStatus.BAD_REQUEST);
         }
         float bugetPropus = chiriePropusaOptional.get();
 
@@ -290,40 +271,33 @@ public class RaportController {
             utilizator.setProfil(profilFinanciar);
             utilizatorService.saveUtilizator(utilizator);
 
-            return new ResponseEntity<>("Suma propusă pentru vacanta a fost acceptată și adăugată.", HttpStatus.OK);
+            return new ResponseEntity<>("Proposed amount for vacation has been accepted and added.", HttpStatus.OK);
         } else {
-            //raportService.removeChiriePropusa(userId);
-            return new ResponseEntity<>("Suma propusă pentru vacanta a fost respinsă.", HttpStatus.OK);
+            return new ResponseEntity<>("Proposed amount for vacation has been declined.", HttpStatus.OK);
         }
     }
 
     @GetMapping("/economii")
     @Operation(summary = "Arata progresul economiilor de la o luna data")
-    public ResponseEntity<Map<String , Float>> getProgres(@RequestHeader("Authorization") String token,
-                                                          @RequestParam("luni") int luni)
-    {
+    public ResponseEntity<Map<String, Float>> getProgres(@RequestHeader("Authorization") String token, @RequestParam("luni") int luni) {
         int userId = jwtutil.getUserIdByToken(token);
         Utilizator utilizator = utilizatorService.getById(userId).get();
 
-        // Obține ultimele diagrame
+        // Obtine ultimele diagrame
         List<Diagrama> diagrame = diagramaService.getUltimeleDiagrame(utilizator, luni);
 
-        // Creează Map-ul rezultat
+        // Creeaza Map-ul rezultat
         Map<String, Float> rezultat = diagrame.stream()
                 .collect(Collectors.toMap(
-                        diagrama -> diagrama.getDataDiagrama().getLuna() + " " + diagrama.getDataDiagrama().getAn(), // Cheia: Luna și anul
+                        diagrama -> diagrama.getDataDiagrama().getLuna() + " " + diagrama.getDataDiagrama().getAn(), // Cheia: Luna si anul
                         diagrama -> {
                             float procent = diagrama.getProcenteCheltuieli().getOrDefault(Cheltuiala.TipCheltuiala.CONTAINER, 0f);
-                            return Math.round(procent * 100) / 100.0f; // Rotunjire la două zecimale
+                            return Math.round(procent * 100) / 100.0f; // Rotunjire la doua zecimale
                         },
-                        (v1, v2) -> v1, // În caz de duplicate (nu ar trebui să fie), ia primul
-                        LinkedHashMap::new // Păstrează ordinea originală
+                        (v1, v2) -> v1, // In caz de duplicate (nu ar trebui sa fie), ia primul
+                        LinkedHashMap::new // Pastreaza ordinea originala
                 ));
 
         return ResponseEntity.ok(rezultat);
     }
-
-
-
-
 }
